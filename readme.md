81d15a85e078dbf841702303329816ef205ca845
Magnus Lilledahl <mlilledahl@gmail.com>
2026-03-12 00:04:37 +0100

    My first commit to amundsen!

---
+++ readme.md
@@ -0,0 +1,3 @@
+This package is small project to explore the universe of Julia.
+
+It is named after the famous Norwegian explorer Roal Amundsen
---
+++ src/amundsen.jl
@@ -0,0 +1,9 @@
+module amundsen
+
+export southpole
+
+function southpole(explorer::String)
+    return "Welcome to the south pole $explorer"
+end
+
+end # module amundsen
